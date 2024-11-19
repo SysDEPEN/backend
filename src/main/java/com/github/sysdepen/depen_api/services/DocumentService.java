e7ccb0aabbd39e2b0c19a16d166e81567caeb50c
kauawerle <kauawerle19@gmail.com>
2024-11-19 15:11:35 -0300

    test

--- src/main/java/com/github/sysdepen/depen_api/services/DocumentService.java
+++ src/main/java/com/github/sysdepen/depen_api/services/DocumentService.java
@@ -2,10 +2,12 @@ package com.github.sysdepen.depen_api.services;
 
 
 import com.github.sysdepen.depen_api.entity.Documents;
+import com.github.sysdepen.depen_api.entity.User;
 import com.github.sysdepen.depen_api.repository.DocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.Document;
 import java.io.File;
 import java.util.List;
 import java.util.Optional;
@@ -16,16 +18,20 @@ public class DocumentService {
     @Autowired
     private DocumentRepository documentRepository;
 
+    @Autowired
+    private UserService userService;
 
-    public Documents save(String userName, String documentType, String fileName) {
+
+    public Documents save(Long userId, String documentType, String fileName) {
 
         try{
             // Define o caminho do arquivo com base no tipo de documento
-            String filePath = "uploads/" + userName + "/" + documentType + "/" + fileName;
+            String filePath = "uploads/" + userId + "/" + documentType + "/" + fileName;
 
             // Cria a entidade e salva no banco
             Documents userDocument = new Documents();
-            userDocument.set(userName);
+            Optional<User> userCurrent =  this.userService.findById(userId);
+            userDocument.setUser(userCurrent.get());
             userDocument.setFilePath(filePath);
             userDocument.setDocumentType(documentType);
 

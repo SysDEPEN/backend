4d8aa5ceddba1c145f742c53b5755e099da3d7aa
kauawerle <kauawerle19@gmail.com>
2024-11-24 15:29:36 -0300

    fix: fixed route

--- src/main/java/com/github/sysdepen/depen_api/controller/DocumentsController.java
+++ src/main/java/com/github/sysdepen/depen_api/controller/DocumentsController.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1/documents")
 public class DocumentsController {
